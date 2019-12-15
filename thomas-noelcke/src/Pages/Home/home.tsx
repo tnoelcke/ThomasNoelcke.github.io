c2c404c322a6d78e1774bd7f313ef42606057e51
tnoelcke <noelcket@oregonstate.edu>
2019-12-15 10:47:10 -0800

    created about me page

--- thomas-noelcke/src/Pages/Home/home.tsx
+++ thomas-noelcke/src/Pages/Home/home.tsx
@@ -21,52 +21,83 @@ const Home: React.FC = () => {
 
             <div className="row">
                 <div className="col-1"></div> 
-                <div className="row">
                 <Tabs defaultActiveKey="about" id="tabs" className="col-10">
                     <Tab eventKey="about" title="About Me">
-                        <div className="col-12 flex">
+                        <div className="text-content-about">
+                            <div className="col-6">
+                            <p>Welcome to my Personal Website!</p>
+
+                            <p>My name is Thomas Noelcke and I'm a software professional committed to providing quality software solutions. I am currently working at the Center for Applied Systems and Software (CASS) at Oregon State University.</p>
+
+                            <p>I started my Computer Science career because of my interest in Atmospheric Science. Part of the curriculum for that computer science class was to take an intro series programming class. After taking this class I was hooked and decided Software Engineering was for me. I then transferred to PSU and later OSU where I completed my BS in Computer Science with a minor in Business and Entrepreneurship.</p>
+
+                            <p>Today at CASS I manage groups of students to complete for hire software work. In this role my responsibilities include, Project Management, Systems Architect, Office Manager, Systems Administration, Software Development, QA Engineer, Systems Analyst and Mentor. I enjoy the challenge that fulfilling multiple roles brings.</p>
+
+                            <p>Technically, I enjoy working on cloud distributed applications. At CASS I’ve worked with clients to produce multiple cloud based solution. I also enjoy creating JavaScript based SPA applications. At CASS I’ve created applications in Reactjs as well as Angular and Vue.js. I enjoy the flexibility and rich functionality that SPAs can provide.</p>
+                            </div>
+                        </div>
+                    </Tab>
+                    <Tab eventKey="experience" title="Experience">
+                        <div className="text-content">
+                        <div className="col-10">
                             <h3>Center For Applied Systems and Software</h3> 
                             <Badge variant="secondary">Systems Development Engineer</Badge>
                             <Badge variant="secondary">June 2018 - Present</Badge>
                         </div>
-                        <div className="col-12">
+                        <div className="col-10">
+                            <ul>
+                                <li>
+                                    <span className="font-weight-bold">Project Management:</span> Organized team of students to plan implement and deploy software projects.
+                                </li>
+                                <li>
+                                    <span className="font-weight-bold">Business Analysis:</span> Worked with clients to analyze existing business processes identifying pain points. Worked with client to propose software solutions that create value.
+                                </li>
+                                <li>
+                                    <span className="font-weight-bold">Systems Architect:</span> Design software systems using best practices to solve business problems in a way that is sustainable and maintainable. 
+                                </li>
+                                <li>
+                                    <span className="font-weight-bold">Office Manager:</span> Forecast project load to determine what adjustments need to be made to staffing levels to meet project demand.
+                                </li>
+                            </ul>
+
+                        </div>
+                        <div className="col-10">
                             <h3>Center For Applied Systems and Software</h3> 
                             <Badge variant="secondary">Student Developer</Badge>
                             <Badge variant="secondary">May 2017 - June 2018</Badge>
                         </div>
-                        <div className="col-12">
+                        <div className="col-10">
                             <ul>
                                 <li>
-                                    <div className="bold">ODOT Response Planning</div> Suite of C#, WinForms and WPF application that creates dynamic messages 
+                                    <span className="font-weight-bold">ODOT Response Planning:</span> Suite of C#, WinForms and WPF application that creates dynamic messages 
                                     for electronic message signs on roads. Created WCF services that communicates with external services.
                                 </li>
                                 <li>
-                                    <div className="bold">ODOT Trip Check Local Entry</div> Created INteractive prototype of an application that allows entry of
+                                    <span className="font-weight-bold">ODOT Trip Check Local Entry: </span> Created Interactive prototype of an application that allows entry of
                                     road events for municipalities. Met with clients to determine basic application requirements and business needs.
                                 </li>
                             </ul>
                         </div>
-                    </Tab>
-                    <Tab eventKey="experience" title="Experience">
-                        <div className="col-12">
-                            Experience Content
                         </div>
                     </Tab>
                     <Tab eventKey="tech" title="Technologies">
-                        <div className="col-12">
-                            My Technologies
+                        <div className="text-content">
+                            <div className="col-8">
+                                My Technologies
+                            </div>
                         </div>
                     </Tab>
-                    <Tab eventKey="education" title="Education">    
-                        <div className="col-12">
-                            My Education
+                    <Tab eventKey="education" title="Education">
+                        <div className="text-content">    
+                            <div className="col-12">
+                                My Education
+                            </div>
                         </div>
                     </Tab>
                 </Tabs>
                 </div>
                 <div className="col-1"></div>
             </div>
-        </div>
     );
 }
 
